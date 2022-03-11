9200b21d5c73fa82e6c39f719bda51768aeb1c32
ThaiNguyen <thainax101@gmail.com>
2022-03-11 15:45:38 +0700

    upsetIntervaldat

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const background = document.querySelector(".cmn-toggle");
 const body = document.querySelector("body");
+const facebook = document.querySelector(".list-link .link-real");
 
 background.addEventListener("click", togglebg);
+facebook.addEventListener("click", linkreal);
 
 function togglebg() {
   body.classList.toggle("active");
+}
+
+function linkreal() {
+  facebook.innerHTML = "Facebook clone";
 }
